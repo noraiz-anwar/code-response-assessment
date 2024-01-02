95fee99c95c4cb357f9f7bb4485812cef37b167b
Itezaz-ul-Hassan <itezaz.hassan@arbisoft.com>
2024-01-02 19:17:04 +0500

    remove v18.12 js

--- openassessment/xblock/code_executor/executors/javascript_code_executors.py
+++ openassessment/xblock/code_executor/executors/javascript_code_executors.py
@@ -3,23 +3,27 @@ from .mixins import ScriptedLanguageExecutorMixin
 from ..interface import CodeExecutor
 
 
-class JavascriptCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
-    docker_image = 'litmustest/code-executor-node:18.12'
-    language = 'javascript'
-    version = 'nodejs-18.12'
-    display_name = 'Javascript (NodeJS 18.12)'
+# class JavascriptCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
+#     docker_image = 'litmustest/code-executor-node:18.12'
+#     language = 'javascript'
+#     version = 'nodejs-18.12'
+#     display_name = 'Javascript (NodeJS 18.12)'
 
-    id = CodeExecutor.create_id('javascript', 'nodejs-18.12')
+#     id = CodeExecutor.create_id('javascript', 'nodejs-18.12')
 
-    SOURCE_FILE_NAME_TEMPLATE = '{name}.js'
-    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'node {source_file}'
-    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'node {source_file} {input_file}'
+#     SOURCE_FILE_NAME_TEMPLATE = '{name}.js'
+#     RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'node {source_file}'
+#     RUN_COMMAND_FILE_INPUT_TEMPLATE = 'node {source_file} {input_file}'
 
 
-class JavascriptCodeExecutorV18(JavascriptCodeExecutor):
+class JavascriptCodeExecutorV18(ScriptedLanguageExecutorMixin, CodeExecutor):
     docker_image = 'litmustest/code-executor-node:18.14.1'
     language = 'javascript'
     version = 'nodejs-18.14.1'
     display_name = 'Javascript (NodeJS 18.14.1)'
 
     id = CodeExecutor.create_id('javascript', 'nodejs-18.14.1')
+
+    SOURCE_FILE_NAME_TEMPLATE = '{name}.js'
+    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'node {source_file}'
+    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'node {source_file} {input_file}'
